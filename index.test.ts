012da0d8ae53fd18db61e74bef5ce3023c061dad
Alexis Janus <janusalexis1@gmail.com>
2025-10-17 15:56:50 +0200

    Mise en place d'un test simple : Créer un produit

--- index.test.ts
+++ index.test.ts
@@ -1,3 +1,9 @@
 import { describe, test, expect } from "@jest/globals"
 
-
+describe('Product', () => {
+  test('Doit créer un produit avec un nom et un prix', () => {
+    const product = new Product("Pomme", 2.50);
+    expect(product.name).toBe("Pomme");
+    expect(product.price).toBe(2.50);
+  });
+});
